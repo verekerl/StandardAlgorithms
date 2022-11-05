8040e21a1f0c8df7e0def0eff32e20f2ffdae63d
Luke Vereker <vereker.l@etoncollege.org.uk>
2022-11-05 11:22:44 +0000

    I ADDED A FUNCTION!!!

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -9,3 +9,22 @@ import Foundation
 
 print("Hello, Bob!")
 
+let question = "How are you today? "
+let message = "\(question)"
+
+print(message)
+
+func number() {
+    let num = 1...10
+    let number = num.randomElement()!
+    print("Guess the number I'm thinking between 1 and 10: ")
+    let user_choice = readLine()
+    var user_number = Int(user_choice)
+    if user_number == number {
+        print("Well done")
+    } else {
+        print("Bye Bye")
+    }
+}
+
+number()
